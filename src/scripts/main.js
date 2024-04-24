3c20bf0efc04cadfb070b64db48bf1545afcdd56
Gabriel Pereira Lisboa <gabriel.lisboadv@gmail.com>
2024-04-23 21:59:05 -0300

    terminado a lógica da contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,30 @@
 AOS.init();
 
-const dataAniversario = new Date("Oct 15, 2024 00:00:00");
-const timeStampAniversario = dataAniversario.getTime();
+const dataAniversario = new Date("Oct 15, 2024 00:00:00");//nova data passando todas as informações
+const timeStampAniversario = dataAniversario.getTime(); //pegando o tempo em milissegundos dessa data
 
-const contaTempo = setInterval(function(){
-    const agora = new Date();
-    const timeStampAtual = agora.getTime();
+const contaTempo = setInterval(function(){//função para definirmos de um em um segundo, oque vamos fazer
+    const agora = new Date(); //instanciando um tempo para quando for efetuada a operação
+    const timeStampAtual = agora.getTime();//pegue o tempo dessa operação atual
 
-    const distanciaAteOEvento = timeStampAniversario - timeStampAtual;
+    const distanciaAteOEvento = timeStampAniversario - timeStampAtual; //lembrando que isso é em milissegundos
 
-    
+    const diasEmMs = 1000 * 60 * 60 * 24;//tempo de dias até o evento sendo transformado para milissegundos e segue a lógica
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+    const segundosEmMs = 1000;
+    //método floor para arredondar para baixo ...
+    const diaAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horasEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / segundosEmMs);
 
-    const diaAteOEvento = Math.floor(distanciaAteOEvento / (1000 * 60 * 60 * 24));
-    const horasAteOEvento = Math.floor(distanciaAteOEvento % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
-    const minutosAteOEvento = distanciaAteOEvento % ()
-    const segundosAteOEvento = Math.floor(distanciaAteOEvento / (1000 * 60));
+    document.getElementById('contador').innerHTML = 
+    `${diaAteOEvento} d ${horasAteOEvento} h ${minutosAteOEvento} min ${segundosAteOEvento} s`;
+
+    if(distanciaAteOEvento<0){
+        clearInterval(contaTempo);
+        document.getElementById('contador').innerHTML = "O evento já terminou!"
+    }
 }
-,1000)
+,1000);
